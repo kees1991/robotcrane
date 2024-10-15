ee3564447bd3db05515582810e5424e505722d0a
keesloeff <keesloeff@gmail.com>
2024-10-15 17:13:05 +0200

    Update AWS URL

--- Frontend/src/communicator.ts
+++ Frontend/src/communicator.ts
@@ -38,7 +38,7 @@ export class Communicator {
 
     get newClientPromise() {
         return new Promise((resolve, reject) => {
-            let wsClient = new WebSocket("ws://ec2-52-45-198-60.compute-1.amazonaws.com:8000/robotcrane");
+            let wsClient = new WebSocket("ws://ec2-52-90-49-194.compute-1.amazonaws.com:8000/robotcrane");
 
             wsClient.onopen = () => {
                 console.log("Connected to websocket");
