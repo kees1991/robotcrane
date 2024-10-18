3cadd1fb63a3a7e078379d2970ea3fbe27c0510a
keesloeff <keesloeff@gmail.com>
2024-10-18 09:15:22 +0200

    Refactor RobotPoseStreamer

--- backend/app/services/RobotPoseStreamer.py
+++ backend/app/services/RobotPoseStreamer.py
@@ -16,6 +16,56 @@ def get_current_time_ms() -> int:
     return round(time() * 1000)
 
 
+def update_robot(robot: RobotCrane, _, trajectory: Trajectory, elapsed_time_in_seconds: float) -> bool:
+    next_act_state = trajectory.next_step(elapsed_time_in_seconds)
+
+    if next_act_state is None:
+        print("No next actuator state found")
+        return False
+
+    robot.set_act_states_t_1(next_act_state)
+
+    return True
+
+
+def update_robot_with_new_origin(robot: RobotCrane, origin_trajectory: OriginTrajectory, trajectory: Trajectory,
+                                 elapsed_time_in_seconds: float) -> bool:
+    # Update robot origin
+    next_origin = origin_trajectory.origin_next_step(elapsed_time_in_seconds)
+    if next_origin is None:
+        print("No next origin found, end streaming.")
+        return False
+    robot.set_origin_t_1(next_origin)
+
+    # Update robot actuator states
+    next_act_state = trajectory.next_step(elapsed_time_in_seconds)
+    if next_act_state is None:
+        print("No next actuator state found, end streaming.")
+        return False
+    robot.set_act_states_t_1(next_act_state)
+
+    return True
+
+
+def update_robot_with_new_origin_and_control_end_effector(robot: RobotCrane, _, simulator: ControlSimulator,
+                                                          elapsed_time_in_seconds: float) -> bool:
+    # Update robot origin
+    next_origin = simulator.origin_next_step(elapsed_time_in_seconds)
+    if next_origin is None:
+        print("No next origin found, end streaming.")
+        return False
+    robot.set_origin_t_1(next_origin)
+
+    # Update robot actuator states
+    next_act_state = simulator.next_step(elapsed_time_in_seconds)
+    if next_act_state is None:
+        print("No next actuator state found, end streaming.")
+        return False
+    robot.set_act_states_t_1(next_act_state)
+
+    return True
+
+
 class RobotPoseStreamer(object):
 
     def __init__(self, websocket: WebSocket, websocket_api: WebSocketAPI):
@@ -26,38 +76,9 @@ class RobotPoseStreamer(object):
 
     async def stream_poses(self, robot: RobotCrane) -> None:
         trajectory = Trajectory(robot)
-
         print(f"Moving time: {trajectory.mov_time}")
 
-        start_time_ms = get_current_time_ms()
-        while True:
-            current_time_ms = get_current_time_ms()
-
-            if not self.should_update_pose(current_time_ms):
-                continue
-
-            elapsed_time_in_seconds = (current_time_ms - start_time_ms) / 1000
-
-            next_act_state = trajectory.next_step(elapsed_time_in_seconds)
-
-            if next_act_state is None:
-                print("No next actuator state found, end streaming.")
-                break
-
-            robot.set_act_states_t_1(next_act_state)
-
-            # Send the pose to the frontend via websocket
-            pose = Pose(robot.get_frames(), robot.origin_t_1, robot.act_states_t_1)
-            await self.websocket_api.send_message(self.websocket, pose.to_json())
-
-            # Yield control to the event loop
-            await asyncio.sleep(0)
-
-            # Update the last time a signal was processed
-            self.last_signal_time_ms = current_time_ms
-
-        # Reset last signal time
-        self.last_signal_time_ms = 0
+        await self.stream(robot, None, trajectory, update_robot)
 
     async def stream_poses_for_new_origin(self, robot: RobotCrane) -> None:
         origin_trajectory = OriginTrajectory(robot.origin_t_0, robot.origin_t_1)
@@ -67,45 +88,15 @@ class RobotPoseStreamer(object):
 
         print(f"Moving time: {trajectory.mov_time}")
 
-        start_time_ms = get_current_time_ms()
-        while True:
-            current_time_ms = get_current_time_ms()
-
-            if not self.should_update_pose(current_time_ms):
-                continue
-
-            elapsed_time_in_seconds = (current_time_ms - start_time_ms) / 1000
-
-            # Update robot origin
-            next_origin = origin_trajectory.origin_next_step(elapsed_time_in_seconds)
-            if next_origin is None:
-                print("No next origin found, end streaming.")
-                break
-            robot.set_origin_t_1(next_origin)
-
-            # Update robot actuator states
-            next_act_state = trajectory.next_step(elapsed_time_in_seconds)
-            if next_act_state is None:
-                print("No next actuator state found, end streaming.")
-                break
-            robot.set_act_states_t_1(next_act_state)
-
-            # Send the pose to the frontend via websocket
-            pose = Pose(robot.get_frames(), robot.origin_t_1, robot.act_states_t_1)
-            await self.websocket_api.send_message(self.websocket, pose.to_json())
-
-            # Yield control to the event loop
-            await asyncio.sleep(0)
-
-            # Update the last time a signal was processed
-            self.last_signal_time_ms = current_time_ms
-
-        # Reset last signal time
-        self.last_signal_time_ms = 0
+        await self.stream(robot, origin_trajectory, trajectory, update_robot_with_new_origin)
 
     async def stream_poses_for_new_origin_and_control_end_effector(self, robot: RobotCrane) -> None:
-        # TODO instantiate OriginTrajectory before Controller
+        # TODO instantiate OriginTrajectory before Controller and pass it to the update function?
         simulator = ControlSimulator(robot)
+
+        await self.stream(robot, None, simulator, update_robot_with_new_origin_and_control_end_effector)
+
+    async def stream(self, robot: RobotCrane, origin_next_step_provider, next_step_provider, update_function) -> None:
 
         start_time_ms = get_current_time_ms()
         while True:
@@ -115,20 +106,9 @@ class RobotPoseStreamer(object):
                 continue
 
             elapsed_time_in_seconds = (current_time_ms - start_time_ms) / 1000
-
-            # Update robot origin
-            next_origin = simulator.origin_next_step(elapsed_time_in_seconds)
-            if next_origin is None:
-                print("No next origin found, end streaming.")
+            if not update_function(robot, origin_next_step_provider, next_step_provider, elapsed_time_in_seconds):
+                print("End streaming.")
                 break
-            robot.set_origin_t_1(next_origin)
-
-            # Update robot actuator states
-            next_act_state = simulator.next_step(elapsed_time_in_seconds)
-            if next_act_state is None:
-                print("No next actuator state found, end streaming.")
-                break
-            robot.set_act_states_t_1(next_act_state)
 
             # Send the pose to the frontend via websocket
             pose = Pose(robot.get_frames(), robot.origin_t_1, robot.act_states_t_1)
